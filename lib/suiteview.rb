65ec09faf69838e0bf406edee365ea469c42413a
BuildTools <unconfigured@null.spigotmc.org>
2023-05-23 11:57:49 -0400

    outline_tag_count method

--- lib/suiteview.rb
+++ lib/suiteview.rb
@@ -1,10 +1,15 @@
 class SuiteView
   attr_accessor :repo
   def initialize(opts)
-    self.repo = opts[:repo]
+    self.repo = CQL::Repository.new(opts[:repo])
   end
 
   def outline_tag_count(tag)
-
+    self.repo.query do
+      select examples
+      transform examples => lambda {|examples| examples[0].rows.drop(1) }
+      from outlines
+      with { || outline.tags.map(&:name).include?(tag)}
+    end.map {|item| item['examples']}.flatten.count
   end
 end
